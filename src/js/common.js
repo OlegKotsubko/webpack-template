3f811245abf63df2ca7355249a3aa28eda6fff88
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-04-24 16:52:02 +0300

    cabinet master branch

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,8 @@
+import customSelect from 'custom-select';
+
+import '../../node_modules/custom-select/src/css/custom-select.css'
 import '../scss/app.scss'
+
+customSelect('select');
+const cstSel =  document.getElementById('mySelect').customSelect
+console.log(cstSel.open); // true|false
