7244da1539b13cfa15de2bedc66027c788656d12
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-04-27 22:36:43 +0300

    weplay contact init

--- src/js/common.js
+++ src/js/common.js
@@ -3,6 +3,7 @@ import customSelect from 'custom-select';
 import '../../node_modules/custom-select/src/css/custom-select.css'
 import '../scss/app.scss'
 
-customSelect('select');
-const cstSel =  document.getElementById('mySelect').customSelect
-console.log(cstSel.open); // true|false
+
+// customSelect('select');
+// const cstSel =  document.getElementById('mySelect').customSelect
+// console.log(cstSel);
