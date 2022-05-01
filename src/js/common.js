ea9d9cf1a5bb84168523b4014625728a754ca975
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-05-01 18:57:45 +0300

    init header and footer 2

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,9 @@
 import menu from "./components/menu";
 import headerScroll from "./components/headerScroll";
+import dropdown from "./components/dropdown";
 
 import '../scss/app.scss'
 
 menu();
 headerScroll();
+dropdown()
