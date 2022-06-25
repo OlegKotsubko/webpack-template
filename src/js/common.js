2de13ad3ef4e58dfb5ded3d0810584d85a30b41a
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-06-25 20:53:46 +0300

    init project

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,9 @@
+import Swiper from 'swiper/bundle';
 import '../scss/app.scss'
+
+const swiper = new Swiper('.swiper',{
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
