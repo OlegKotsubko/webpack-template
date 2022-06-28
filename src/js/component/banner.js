29b762af7130ca1390a10014cd08978f82b0fb4c
Oleg Kotsubko <olegkotsubko@gmail.com>
2022-06-28 21:18:07 +0300

    fix banner and menu

--- src/js/component/banner.js
+++ src/js/component/banner.js
@@ -9,17 +9,26 @@ const banner = () => {
   const decorOne = document.querySelector('.js-banner-decor-one')
   const decorTwo = document.querySelector('.js-banner-decor-two')
 
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      trigger: banner,
-      start: `center bottom`,
-      scrub: 2.5,
+  ScrollTrigger.matchMedia({
+    "(max-width: 1279px)": function () {
+      banner.removeAttribute('style');
+      decorOne.removeAttribute('style');
+      decorTwo.removeAttribute('style');
     },
-  })
+    "(min-width: 1280px)": function () {
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: banner,
+          start: `center bottom`,
+          scrub: 2.5,
+        },
+      })
 
-  tl.addLabel("start")
-    .from(decorOne, { y: 50}, "start")
-    .from(decorTwo, { y: 100}, "start")
+      tl.addLabel("start")
+        .from(decorOne, { y: 50}, "start")
+        .from(decorTwo, { y: 100}, "start")
+    }
+  })
 }
 
 export default banner
